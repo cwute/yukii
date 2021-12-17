4f23192cfc958213fc9be8fc90426536ad292aff
Liko <martiskat05@gmail.com>
2021-12-17 20:44:16 +0100

    added tommy, custom added some debug for myself so i can check few thing while compensating recoil and added key list to menu.

--- sfsefsfsf.cpp
+++ sfsefsfsf.cpp
@@ -41,7 +41,8 @@ void Mouse_Move(Vector2 vals)
     mouse_event(MOUSEEVENTF_MOVE, (int)vals.x, (int)vals.y, 0, 0);
 }
 
-void Smoothing(float delay, float animation, Vector2 in,bool holo,bool scope8x,bool suppresor) {
+void Smoothing(float delay, float animation, Vector2 in,bool holo,bool scope8x,bool suppresor, int magsize) {
+    bool debug = true;
     int x_ = 0, y_ = 0, t_ = 0;
     int xmult, ymult;
     if (holo && !suppresor) {
@@ -72,6 +73,11 @@ void Smoothing(float delay, float animation, Vector2 in,bool holo,bool scope8x,b
         xmult = xmult + rand() % 2 / 2;
         ymult = ymult + rand() % 2 / 2;
     }
+    if (debug) {
+        std::cout << "Current bullet: " << magsize << std::endl;
+        std::cout << "Current Vector2: x: " << in.x << " y: " << in.y << std::endl;
+        std::cout << "Calculated Vector2: x:" << xmult << " y: " << ymult << std::endl;
+    }
     for (int i = 1; i <= (int)animation; ++i) {
         int xI = i * xmult / (int)animation;
         int yI = i * ymult / (int)animation;
@@ -81,6 +87,7 @@ void Smoothing(float delay, float animation, Vector2 in,bool holo,bool scope8x,b
         x_ = xI; y_ = yI; t_ = tI;
     }
     QuerySleep((int)delay - (int)animation);
+
 }
 
 void printStart() {
@@ -129,6 +136,20 @@ void print(boolean holo, boolean scope8x,int choice, bool suppresor) {
 \|___|/                                        
                                                
                                                )" << '\n';
+    SetConsoleTextAttribute(hConsole, 14);
+    std::cout << "Keys: " << std::endl;
+    std::cout << "AK = END" << std::endl;
+    std::cout << "MP5 = PAGE UP" << std::endl;
+    std::cout << "M249 = PAGE DOWN" << std::endl;
+    std::cout << "SEMI = HOME " << std::endl;
+    std::cout << "M39 = INSERT " << std::endl;
+    std::cout << "LR = DELETE" << std::endl;
+    std::cout << "TOMMY = ALT + DELETE" << std::endl;
+    std::cout << "CUSTOM = ALT + INSERT" << std::endl;
+    std::cout << "HOLO = F9" << std::endl;
+    std::cout << "SCOPE 8X = F10" << std::endl;
+    std::cout << "SUPPRESOR = ALT + F9 " << std::endl << std::endl;
+
     SetConsoleTextAttribute(hConsole, 11);
     std::cout << "Weapon: " << "                         ";
     switch (choice) {
@@ -137,23 +158,25 @@ void print(boolean holo, boolean scope8x,int choice, bool suppresor) {
              SetConsoleTextAttribute(hConsole, 10);
              std::cout << "none, ";
              SetConsoleTextAttribute(hConsole, 12);
-             std::cout << "ak, mp5, m249, semi, m39, lr" << std::endl;
+             std::cout << "ak, mp5, m249, semi, m39, lr, tommy, custom" << std::endl;
               break;
 
          case 1:
+             SetConsoleTextAttribute(hConsole, 12);
+             std::cout << "none, ";
              SetConsoleTextAttribute(hConsole, 10);
              std::cout << "ak, ";
              SetConsoleTextAttribute(hConsole, 12);
-             std::cout << "mp5, m249, semi, m39, lr" << std::endl;
+             std::cout << "mp5, m249, semi, m39, lr, tommy, custom" << std::endl;
               break;
 
          case 2:
              SetConsoleTextAttribute(hConsole, 12);
-             std::cout << "ak, ";
+             std::cout << "none, ak, ";
              SetConsoleTextAttribute(hConsole, 10);
              std::cout << "mp5, ";
              SetConsoleTextAttribute(hConsole, 12);
-             std::cout << "m249, semi, m39, lr" << std::endl;
+             std::cout << "m249, semi, m39, lr, tommy, custom" << std::endl;
               break;
 
          case 3:
@@ -162,31 +185,46 @@ void print(boolean holo, boolean scope8x,int choice, bool suppresor) {
              SetConsoleTextAttribute(hConsole, 10);
              std::cout << "m249, ";
              SetConsoleTextAttribute(hConsole, 12);
-             std::cout << "semi, m39, lr" << std::endl;
+             std::cout << "semi, m39, lr, tommy, custom" << std::endl;
               break;
 
          case 4:
              SetConsoleTextAttribute(hConsole, 12);
-             std::cout << "ak, mp5, m249, ";
+             std::cout << "none, ak, mp5, m249, ";
              SetConsoleTextAttribute(hConsole, 10);
              std::cout << "semi, ";
              SetConsoleTextAttribute(hConsole, 12);
-             std::cout << "m39, lr" << std::endl;
+             std::cout << "m39, lr, tommy, custom" << std::endl;
               break;
          case 5:
              SetConsoleTextAttribute(hConsole, 12);
-             std::cout << "ak, mp5, m249, semi, ";
+             std::cout << "none, ak, mp5, m249, semi, ";
              SetConsoleTextAttribute(hConsole, 10);
              std::cout << "m39, ";
              SetConsoleTextAttribute(hConsole, 12);
-             std::cout << "lr" << std::endl;
+             std::cout << "lr, tommy, custom" << std::endl;
              break;
          case 6:
              SetConsoleTextAttribute(hConsole, 12);
-             std::cout << "ak, mp5, m249, semi, m39, ";
+             std::cout << "none, ak, mp5, m249, semi, m39, ";
              SetConsoleTextAttribute(hConsole, 10);
-             std::cout << "lr" << std::endl;
-
+             std::cout << "lr,";
+             SetConsoleTextAttribute(hConsole, 12);
+             std::cout << " tommy, custom " << std::endl;
+             break;
+         case 7:
+             SetConsoleTextAttribute(hConsole, 12);
+             std::cout << "none, ak, mp5, m249, semi, m39, lr, ";
+             SetConsoleTextAttribute(hConsole, 10);
+             std::cout << "tommy, ";
+             SetConsoleTextAttribute(hConsole, 12);
+             std::cout << " custom " << std::endl;
+             break;
+         case 8:
+             SetConsoleTextAttribute(hConsole, 12);
+             std::cout << "none, ak, mp5, m249, semi, m39, lr, tommy, ";
+             SetConsoleTextAttribute(hConsole, 10);
+             std::cout << "custom " << std::endl;
              break;
     }
 
@@ -238,9 +276,9 @@ int main()
     boolean holo = false;
     boolean scope8x = false;
     boolean suppresor = false;
+    boolean debug = true;
 
     while (true) {
-        
         int magsize = 0;
         if (GetAsyncKeyState(VK_END) & 0x8000) {
             if (choice == 1) {
@@ -307,7 +345,7 @@ int main()
 
             }
         }
-        if (GetAsyncKeyState(VK_INSERT) & 0x8000) {
+        if (GetAsyncKeyState(VK_INSERT) & 0x8000 && !(GetAsyncKeyState(VK_MENU) & 0x8000)) {
 
             if (choice == 5) {
                 choice = 0;
@@ -325,7 +363,7 @@ int main()
 
             }
         }
-        if (GetAsyncKeyState(VK_DELETE) & 0x8000) {
+        if (GetAsyncKeyState(VK_DELETE) & 0x8000 && !(GetAsyncKeyState(VK_MENU) & 0x8000)) {
 
             if (choice == 6) {
                 choice = 0;
@@ -343,7 +381,7 @@ int main()
 
             }
         }
-        if (GetAsyncKeyState(VK_F7) & 0x8000) {
+        if (GetAsyncKeyState(VK_F9) & 0x8000 && GetAsyncKeyState(VK_MENU) & 0x8000) {
             if (suppresor) {
                 Beep(1000, 100);
                 suppresor = false;
@@ -359,7 +397,7 @@ int main()
                 Sleep(500);
             }
         }
-        if (GetAsyncKeyState(VK_F9) & 0x8000) {
+        if (GetAsyncKeyState(VK_F9) & 0x8000 && !(GetAsyncKeyState(VK_MENU) & 0x8000)) {
             if (holo) {
                 Beep(1000, 100);
                 holo = false;
@@ -395,6 +433,40 @@ int main()
                 Sleep(500);
             }
         }
+        if(GetAsyncKeyState(VK_MENU) & 0x8000 && GetAsyncKeyState(VK_DELETE)){
+            if (choice == 7) {
+                choice = 0;
+                Beep(1000, 100);
+                system("CLS");
+                print(holo, scope8x, choice, suppresor);
+                Sleep(500);
+            }
+            else {
+                choice = 7;
+                Beep(800, 100);
+                system("CLS");
+                print(holo, scope8x, choice, suppresor);
+                Sleep(500);
+
+            }
+        }
+        if (GetAsyncKeyState(VK_MENU) & 0x8000 && GetAsyncKeyState(VK_INSERT)){
+            if (choice == 8) {
+                choice = 0;
+                Beep(1000, 100);
+                system("CLS");
+                print(holo, scope8x, choice, suppresor);
+                Sleep(500);
+            }
+            else {
+                choice = 8;
+                Beep(800, 100);
+                system("CLS");
+                print(holo, scope8x, choice, suppresor);
+                Sleep(500);
+
+            }
+        }
         if (GetAsyncKeyState(VK_F12) & 0x8000) exit(0);
 
         switch (choice) {
@@ -405,7 +477,7 @@ int main()
             while (GetAsyncKeyState(VK_LBUTTON) & 0x8000 && GetAsyncKeyState(VK_RBUTTON) & 0x8000) {
 
                 if (magsize < 31) {
-                    Smoothing(133.f, AssaultRifleControl::control_ak[magsize], AssaultRifle::data[magsize],holo,scope8x,suppresor);
+                    Smoothing(133.f, AssaultRifleControl::control_ak[magsize], AssaultRifle::data[magsize],holo,scope8x,suppresor,magsize);
                     magsize++;
                 }
             }
@@ -414,7 +486,7 @@ int main()
             //mp5
             while (GetAsyncKeyState(VK_LBUTTON) & 0x8000 && GetAsyncKeyState(VK_RBUTTON) & 0x8000) {
                 if (magsize < 31) {
-                    Smoothing(100.f, MP5Control::control_mp5[magsize], MP5::data[magsize], holo,scope8x,suppresor);
+                    Smoothing(100.f, MP5Control::control_mp5[magsize], MP5::data[magsize], holo,scope8x,suppresor, magsize);
                     magsize++;
                 }
             }
@@ -423,7 +495,7 @@ int main()
             //m249
             while (GetAsyncKeyState(VK_LBUTTON) & 0x8000 && GetAsyncKeyState(VK_RBUTTON) & 0x8000) {
                 if (magsize < 100) {
-                    Smoothing(110.f, 110.f, M249::data[magsize], holo,scope8x,suppresor);
+                    Smoothing(110.f, 110.f, M249::data[magsize], holo,scope8x,suppresor, magsize);
                     magsize++;
                 }
             }
@@ -432,7 +504,7 @@ int main()
             //semi
             while (GetAsyncKeyState(VK_LBUTTON) & 0x8000 && GetAsyncKeyState(VK_RBUTTON) & 0x8000) {
                 if (magsize < 100) {
-                    Smoothing(103.f, 103.f, semi::data[0], holo, scope8x,suppresor);
+                    Smoothing(103.f, 103.f, semi::data[0], holo, scope8x,suppresor, magsize);
                     magsize++;
                 }
             }
@@ -441,7 +513,7 @@ int main()
             //m39
             while (GetAsyncKeyState(VK_LBUTTON) & 0x8000 && GetAsyncKeyState(VK_RBUTTON) & 0x8000) {
                 if (magsize < 100) {
-                    Smoothing(205.f, 205.f, M39::data[0], holo, scope8x, suppresor);
+                    Smoothing(205.f, 205.f, M39::data[0], holo, scope8x, suppresor, magsize);
                     magsize++;
                 }
             }
@@ -451,12 +523,33 @@ int main()
             while (GetAsyncKeyState(VK_LBUTTON) & 0x8000 && GetAsyncKeyState(VK_RBUTTON) & 0x8000) {
 
                   if (magsize < 31) {
-                      Smoothing(120.f, LRControl::control_lr[magsize], LR::data[magsize], holo, scope8x, suppresor);
+                      Smoothing(120.f, LRControl::control_lr[magsize], LR::data[magsize], holo, scope8x, suppresor, magsize);
                       magsize++;
                   }
             }
             break;
+        case 7:
+            //tommy
+            while (GetAsyncKeyState(VK_LBUTTON) & 0x8000 && GetAsyncKeyState(VK_RBUTTON) & 0x8000) {
+                if (magsize < 21) {
+                    Smoothing(129.f, tommyControl::control_tommy[magsize], tommy::data[magsize],holo,scope8x,suppresor, magsize);
+                    magsize++;
+                    }
+            }
+            break;
+        case 8:
+            //custom
+            while (GetAsyncKeyState(VK_LBUTTON) & 0x8000 && GetAsyncKeyState(VK_RBUTTON) & 0x8000) {
+                if (magsize < 21) {
+                    Smoothing(100.f, customControl::control_custom[magsize], custom::data[magsize], holo, scope8x, suppresor, magsize);
+                    magsize++;
+                }
+            }
+
+            break;
         }
+    
+
     }
 }
 
